2af6c475f74e959b9a5732f9af8aa180b0f8d964
Eli Mullan <EliMullan21@gmail.com>
2022-03-21 08:49:52 +0000

    add playlist name change functionality

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -3,10 +3,20 @@ import './Playlist.css';
 import { TrackList } from '../TrackList/TrackList';
 
 export class Playlist extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event) {
+        const newPlaylistName = event.target.value;
+        this.props.onNameChange(newPlaylistName);
+    }
+
     render() {
         return(
             <div className="Playlist">
-            <input defaultValue = {'New Playlist'} />
+            <input type='text' defaultValue = {'New Playlist'} onChange={this.handleNameChange} />
             <TrackList  tracks={this.props.playlistTracks} 
             onRemove={this.props.onRemove}
             isRemoval={true} />
